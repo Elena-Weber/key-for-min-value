79d979025f082367774620509aa5e52aaf0a590c
Elena-Weber <crochetmiracles@gmail.com>
2021-02-28 19:06:22 -0500

    commit tree 8b4d7f1601e641677d28c40ef42ddaf934d60a4d

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
+
+
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+min_name = nil
+min_amount = nil
+name_hash.each do |name, amount|
+        if min_amount == nil
+            min_amount = amount
+            min_name = name
+        elsif amount < min_amount
+            min_amount = amount
+            min_name = name
+        end
+    end
+    min_name
 end
